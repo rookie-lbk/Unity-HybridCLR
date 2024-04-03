b2317b48a70c70625c89a7723017104870649163
liubenkang <benkang.liu@centurygame.com>
2024-04-03 16:09:24 +0800

    no message

--- Project/Assets/HotUpdate/Hello.cs
+++ Project/Assets/HotUpdate/Hello.cs
@@ -5,7 +5,7 @@ public class Hello
 {
     public static void Run()
     {
-        // Debug.Log("Hello, HybridCLR");
-        Debug.Log("Hello, World");
+        Debug.Log("Hello, HybridCLR");
+        // Debug.Log("Hello, World");
     }
 }
